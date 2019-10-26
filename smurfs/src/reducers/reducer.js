fc09d12190489865367ef2ead36c62313b2b960a
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-26 12:27:41 -0400

    created starter reducer

---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,13 @@
+const initialState = [{
+    "name":"Brainey",
+    "age":200,
+    "height":"5cm",
+    "id":0
+}]
+
+export const reducer = (state = initialState  , action) => {
+    switch () {
+        default: 
+            return state;
+    }
+}
