51f76e0893e78da9cb07b4c4fd95dc51d7d900c3
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-28 22:44:50 -0400

    created my changeHandlers. MVP Complete

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { addSmurf } from '../actions/action';
 
-const SmurfForm = () => {
+const SmurfForm = props => {
+
+    const [smurf, setSmurf] = useState({name: '', height: '', age: ''})
+
+    const handleChange = (e) => {
+        setSmurf({
+            ...smurf,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        props.addSmurf({
+            ...smurf, 
+            age: parseInt(smurf.age, 10)
+        })
+        setSmurf({name: '', height: '', age: ''})
+    }
+
+
+
     return (
-        <div>
-            <input type='text' placeholder='Smurf name'/>
-            <input type='text' placeholder='Smurf height'/>
-            <input type='text' placeholder='Smurf age'/>
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Smurf name' name='name' value={smurf.name} onChange={handleChange}/>
+            <input type='text' placeholder='Smurf height' name='height' value={smurf.height} onChange={handleChange}/>
+            <input type='text' placeholder='Smurf age' name='age' value={smurf.age} onChange={handleChange}/>
             <button>Submit Smurf</button>
-        </div>
+        </form>
     )
 }
 
-export default SmurfForm
+const mapStateToProps = state => ({
+    addSmurf: state.addSmurf
+});
+
+export default connect(mapStateToProps, { addSmurf })(SmurfForm);
