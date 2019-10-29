80249b86cbc4e269e6eaba646d502b320e67f605
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-28 22:43:09 -0400

    made post request to add smurf

--- smurfs/src/actions/action.js
+++ smurfs/src/actions/action.js
@@ -4,6 +4,9 @@ export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE';
 
+export const ADD_SMURF_START = 'ADD_SMURF_START';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAILURE = 'ADD_SMURF_FAILURE';
 
 export const getSmurfs = () => dispatch => {
     dispatch({ type: FETCH_SMURFS_START });
@@ -16,4 +19,17 @@ export const getSmurfs = () => dispatch => {
             console.log(err.response)
             dispatch({ type: FETCH_SMURFS_FAILURE, payload: err.response })
         });
+}
+
+export const addSmurf = smurf => dispatch => {
+    dispatch({ type: ADD_SMURF_START });
+    axios.post('http://localhost:3333/smurfs', smurf)
+        .then(res => {
+            console.log('added Smurf:', res)
+            dispatch({ type: ADD_SMURF_SUCCESS, payload: res.data})
+        })
+        .catch(err => {
+            console.log('Added Smurf Fail:', err)
+            dispatch({ type: ADD_SMURF_FAILURE, payload: err.response})
+        })
 }
