c00fbf94197b61264c8e3be1aad2e13e053a806c
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-28 22:44:00 -0400

    added my cases for adding smurf

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,4 +1,4 @@
-import {FETCH_SMURFS_START, FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FAILURE} from '../actions/action'
+import {FETCH_SMURFS_START, FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FAILURE, ADD_SMURF_START, ADD_SMURF_SUCCESS, ADD_SMURF_FAILURE} from '../actions/action'
 
 const initialState = {
     smurfs: [],
@@ -20,13 +20,31 @@ export const reducer = (state = initialState  , action) => {
                 error: '',
                 isFetching: false,
                 smurfs: action.payload
-            }
+            };
         case FETCH_SMURFS_FAILURE: 
             return {
                 ...state,
                 error: action.payload,
                 isFetching: false
             };
+        case ADD_SMURF_START:
+            return {
+                ...state,
+                error: '',
+                isFetching: true
+            };
+        case ADD_SMURF_SUCCESS: 
+            return {
+                ...state,
+                error: '',
+                isFetching: false
+            };
+        case ADD_SMURF_FAILURE: 
+            return {
+                ...state,
+                error: action.payload,
+                isFetching: false
+            }
         default: 
             return state;
     }
